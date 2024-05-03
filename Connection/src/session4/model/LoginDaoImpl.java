5587d07398f45ea2b1b1b41d160d939e00e95fad
kyozun <daicacuong0311@gmail.com>
2024-05-03 11:06:29 +0700

    Connection

--- Connection/src/session4/model/LoginDaoImpl.java
+++ Connection/src/session4/model/LoginDaoImpl.java
@@ -2,13 +2,43 @@ package session4.model;
 
 import session4.entity.Users;
 
-import java.sql.SQLException;
+import java.sql.*;
 
 public class LoginDaoImpl implements LoginDAO {
+    private static final Connection connection;
+
+    static {
+        try {
+            connection = MySQLConnectionDB.getMySQLConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
 
     @Override
     public String checkLoginStatement(Users user) throws SQLException {
-        return "";
+
+        String username = user.getUser_name();
+        String password = user.getPassword();
+        //Query
+        String query = "select username from users where username like '"+username+"' and password like '"+password+"'";
+
+        //Tạo statement mỗi lần thực thi
+        statement = connection.createStatement();
+
+        //Tạo đối tượng ResultSet để nhận kết quả từ database trả về
+        ResultSet resultSet = statement.executeQuery(query);
+
+        //Lặp qua toàn bộ bản ghi
+        while (resultSet.next()) {
+            System.out.println("Username is: " + resultSet.getString("username"));
+            return resultSet.getString("username");
+        }
+
+        return "Not in the database";
     }
 
     @Override
